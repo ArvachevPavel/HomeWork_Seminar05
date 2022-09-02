eddb5dfaf72ac78d870474aca5fb5ed6482f2a85
Pavel Arvachev <arvachev@gmail.com>
2022-09-02 19:04:30 +0300

    Add task 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,27 +1,47 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-Console.WriteLine("Введите количество элементов массива");
-int a = int.Parse(Console.ReadLine());
 
-int [] collection = new int[a];
-
-int length = collection.Length;
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
     int index = 0;
     while(index<length)
     {
         collection[index] = new Random().Next(100,1000);
         index++;
     }
-int count = 0;
-int i = 0;
-while (i<length)
-{
-    if (collection[index]%2 == 0)
-    {
-        count = count + 1;
-    }
-    i++;
 }
-Console.WriteLine(count);
+void PrintArray(int [] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введите количество элементов массива");
+int a = int.Parse(Console.ReadLine());
+int [] array = new int[a];
+
+FillArray (array);
+PrintArray (array);
+
+int length1 = array.Length;
+int j = 0;
+int count1 = 0;
+while (j<length1)
+{
+    if(array[j]%2 == 0)
+    {
+        count1 = count1 + 1;
+    }
+j++;
+}
+Console.Write(" -> " + count1);
+
 
 
 
