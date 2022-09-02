f8a6176992ebce5ece93c27b821c790214b965b3
Pavel Arvachev <arvachev@gmail.com>
2022-09-02 19:49:57 +0300

    Add task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-100 , 100);
+        index++;
+    }
+}
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введите количество элементов массива");
+int a = int.Parse(Console.ReadLine());
+int[] array = new int[a];
+
+FillArray(array);
+PrintArray(array);
+int b = 0;
+int i = 1;
+while (i < array.Length)
+{
+    b = b + array[i];
+    i = i + 2;
+}
+
+
+Console.Write (" -> " + b);
+
+
+
+
+
